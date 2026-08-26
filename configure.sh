336d7f743c5d5743bbeff668cda2ed90ada57f1e
JoyenBenitto <joyen.benitto12@gmail.com>
2026-08-26 13:40:41 +0530

    init

---
+++ configure.sh
@@ -0,0 +1,3 @@
+mkdir -p build
+cd build; cmake .. #configure the project
+cmake --build . #build the project
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Your code here
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
